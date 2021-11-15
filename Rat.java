331bf7c0e92a12e90c4ce9cd9e41a54b7eeb16b6
griffinslt <slt.griffin3105@gmail.com>
2021-11-15 15:45:04 +0000

    Opened the repository in intelliJ and it added these files to make it work

---
+++ Rat.java
@@ -0,0 +1,25 @@
+/**
+ * This Class represents a rat in the game.
+ * @author samgriffin
+ */
+public class Rat {
+    private double speed;
+    private int ratID;
+
+    public Rat() {
+        speed = 0;
+        ratID = 0;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public int getRatID() {
+        return ratID;
+    }
+
+    public String toString(){
+        return String.valueOf(speed) + ", " + String.valueOf(ratID);
+    }
+}
