5eb9002194ea647651da2574ae82e2d7bfa944c9
RyanWakey <wake52@outlook.com>
2021-11-23 19:55:24 +0000

    Create PlayerProfile.java
    Created PlayerProfile Class with related methods

---
+++ PlayerProfile.java
@@ -0,0 +1,48 @@
+/**
+ * This Class represents a Players Profile.
+ * @author ryanwake
+ */
+public class PlayerProfile {
+
+    private String playerName;
+    private int maxLevelCompleted;
+
+    /**
+     * Constructor when initializing a new User
+     */
+    public PlayerProfile(String playerName) {
+        setPlayerName(playerName);
+    }
+    /**
+     * Constructor when importing an existing User
+     */
+    public PlayerProfile(String playerName, int maxLevelCompleted){
+        setPlayerName(playerName);
+        setMaxLevelCompleted(maxLevelCompleted);
+    }
+
+    public void incrementLevelCompleted(){
+        maxLevelCompleted =+ 1;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public void setMaxLevelCompleted(int maxLevelCompleted) {
+        this.maxLevelCompleted = maxLevelCompleted;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getMaxLevelCompleted() {
+        return maxLevelCompleted;
+    }
+
+    @Override
+    public String toString() {
+        return playerName + ", " + String.valueOf(maxLevelCompleted);
+    }
+}
