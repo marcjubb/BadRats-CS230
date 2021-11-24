e3f9ccc7893d46943d66eb90c366ada1a0794611
kiruna20 <72553502+kiruna20@users.noreply.github.com>
2021-11-24 19:49:15 +0000

    Update Rat.java

--- Rat.java
+++ Rat.java
@@ -1,14 +1,16 @@
 /**
  * This Class represents a rat in the game.
- * @author samgriffin
+ * @author samgriffin and Alex Walker
  */
 public class Rat {
     protected double speed;
-    private int ratID;
+    private final int RAT_ID; 
+    private static int numberOfRats = 0;
 
     public Rat() {
+        //Set ID
+        RAT_ID = numberOfRats++;
         speed = 0;
-        ratID = 0;
     }
 
     public double getSpeed() {
