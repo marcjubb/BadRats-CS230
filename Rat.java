cd5dd3547a78df2a45be2e619b8ccdc32c3b2808
griffinslt <slt.griffin3105@gmail.com>
2021-11-25 20:20:17 +0000

    Updated Rat with changed constructor and began to add some extra method

--- Rat.java
+++ Rat.java
@@ -1,27 +1,40 @@
+import java.util.Random;
+
 /**
  * This Class represents a rat in the game.
+ *
  * @author samgriffin and Alex Walker
  */
-public class Rat {
-    protected double speed;
-    private final int RAT_ID; 
-    private static int numberOfRats = 0;
+
+/*TODO Remove RAT_ID and related references
+ *  create functioning move method, which should update its location*/
+public class Rat extends VisibleObject {
+    protected int speed;
 
     public Rat() {
-        //Set ID
-        RAT_ID = numberOfRats++;
-        speed = 0;
+        super(0, 0);//0 is just a placeholder value it should actually be randomly generated
+        speed = 0; //0 is  placeholder value for whatever value we decide on in the end
     }
+
+    public Rat(int x, int y, int speed) {
+        super(x, y);
+        this.speed = speed;
+    }
+
 
     public double getSpeed() {
         return speed;
     }
 
-    public int getRatID() {
-        return ratID;
+    private void generateRandomX(){
+        //return new Random().nextInt(Level.getLevelHeight());
     }
 
-    public String toString(){
-        return String.valueOf(speed) + ", " + String.valueOf(ratID);
+    private void generateRandomY(){
+        //return
+    }
+
+    public String toString() {
+        return String.valueOf(speed) + ", " + x + ", " + y;
     }
 }
