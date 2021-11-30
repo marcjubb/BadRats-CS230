6636b0786715bb8905391c3ed1a7eac3ba4f86d3
griffinslt <slt.griffin3105@gmail.com>
2021-11-30 16:35:54 +0000

    Started to implement move method

--- Rat.java
+++ Rat.java
@@ -9,6 +9,8 @@ import java.util.Random;
 /*TODO create functioning move method, which should update its location - need to wait to know whether we can make level static*/
 public class Rat extends VisibleObject {
     protected int speed;
+    private enum Direction {NORTH, SOUTH, EAST, WEST}
+    private Direction direction = Direction.EAST; //this should probably randomly generated in the constructor
 
     //I think this constructor is redundant but i'm not sure - I'll wait for a second opinion
     public Rat(int x, int y, int speed) {
@@ -23,6 +25,34 @@ public class Rat extends VisibleObject {
 
     public double getSpeed() {
         return speed;
+    }
+
+    public void move(){
+        switch (direction) {
+            case NORTH:
+                //
+
+
+            case SOUTH:
+                //
+
+
+            case EAST:
+                if (Level.getLevelLayout()[x + 1][y] != 'G') {
+                    moveRight();
+                } else {
+                    if (Level.getLevelLayout()[x][y + 1] != 'G') {
+                        moveUp();
+                        direction = Direction.NORTH;
+                    } else if (Level.getLevelLayout()[x][y - 1] != 'G') {
+                        moveDown();
+                        direction = Direction.SOUTH;
+                    } else {
+                        moveLeft();
+                        direction = Direction.WEST;
+                    }
+                }
+        }
     }
 
     public void moveRight(){
