ce7fef19e003dbfba3f7225f17bc512f923322aa
RyanWakey <wake52@outlook.com>
2021-12-05 06:38:45 +0000

    javadoc/coding conventions, inventory that could help for determing N.O items currently in list

--- Image.java
+++ Image.java
@@ -12,55 +12,64 @@ class ItemImage {
     protected static Image Bomb3;
     protected static Image Bomb2;
     protected static Image Bomb1;
+    protected static Image Bomb;
     protected static Image Gas;
     protected static Image FemaleSexChange;
     protected static Image MaleSexChange;
     protected static Image Poison;
     protected static Image NoEntry;
     protected static Image Sterilisation;
+    protected static Image DeathRatItem;
 
-    static {
+    public ItemImage(){
 
         Bomb4 = new Image("resources/Bomb4.png");
         Bomb3 = new Image("resources/Bomb3.png");
         Bomb2 = new Image("resources/Bomb2.png");
-        Bomb1 = new Image("resources/Bomb1.png");
-        Gas = new Image("resources/Gas.png");
-        FemaleSexChange = new Image("resources/FemaleSexChange.png");
-        MaleSexChange = new Image("resources/MaleSexChange.png");
-        Poison = new Image("resources/Poison.png");
-        NoEntry = new Image("resources/NoEntry.png");
-        Sterilisation = new Image("resources/Sterilisation.png");
+        Bomb = new Image("resources/Images/Items/Bomb.png");
+        Gas = new Image("resources/Images/Items/Gas.png");
+        FemaleSexChange = new Image("resources/Images/Items/Gas.png");
+        MaleSexChange = new Image("resources/Images/Items/MaleSexChange.png");
+        Poison = new Image("resources/Images/Items/Poison.png");
+        NoEntry = new Image("resources/Images/Items/NoEntry.png");
+        Sterilisation = new Image("resources/Images/Items/Sterilisation.png");
+        DeathRatItem = new Image("resources/Images/Rat/DeathRatR.png");
     }
-    public static Image Bomb4(){
+    public static Image Bomb4() {
         return Bomb4;
     }
-    public static Image Bomb3(){
+    public static Image Bomb3() {
         return Bomb3;
     }
-    public static Image Bomb2(){
+    public static Image Bomb2() {
         return Bomb2;
     }
-    public static Image Bomb1(){
+    public static Image Bomb1() {
         return Bomb1;
     }
-    public static Image Gas(){
+    public static Image getBomb() {
+        return Bomb;
+    }
+    public static Image getGas() {
         return Gas;
     }
-    public static Image getFemaleSexChangeImage(){
+    public static Image getFemaleSexChangeImage() {
         return FemaleSexChange;
     }
-    public static Image getMaleSexChangeImage() {
+    public static Image getMaleSexChangeImage()  {
         return MaleSexChange;
     }
-    public static Image Poison(){
+    public static Image getPoison() {
         return Poison;
     }
-    public static Image NoEntry(){
+    public static Image getNoEntry() {
         return NoEntry;
     }
-    public static Image Sterilisation(){
+    public static Image getSterilisation() {
         return Sterilisation;
+    }
+    public static Image getDeathRatItem() {
+        return DeathRatItem;
     }
 
 
