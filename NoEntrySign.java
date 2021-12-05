17ad28e911fd4126fd95655e5b2cf3ce86c7d54f
RyanWakey <wake52@outlook.com>
2021-12-05 00:10:06 +0000

    javadoc

--- NoEntrySign.java
+++ NoEntrySign.java
@@ -5,15 +5,22 @@ import javafx.scene.image.Image;
  * Blocks rat movement until it takes enough damage and is destroyed.
  * @author Michael Pokorski
  */
-
 public class NoEntrySign extends Item {
     private int durability;
 
+    /**
+     * Creates No entry sign at specified coordinate.
+     * @param x The x coordinate.
+     * @param y The y coordinate.
+     */
     public NoEntrySign(int x, int y) {
         super(x, y, "NoEntrySign", "/resources/Images/Items/NoEntry.png");
         durability = 5;
     }
 
+    /**
+     * Will destroy the No Entry Sign after specified number of collisions
+     */
     public void damage() {
         durability--;
         if (durability <= 0) {
@@ -22,6 +29,10 @@ public class NoEntrySign extends Item {
         }
     }
 
+    /**
+     * Draw the No Entry Sign at each durability on the canvas.
+     * @param gc The GraphicsContext in which we will draw on
+     */
     public void draw(GraphicsContext gc) {
         if (durability == 5) {
             gc.drawImage(new Image("/resources/Images/Items/NoEntry.png"), x * tileSize, y * tileSize);
